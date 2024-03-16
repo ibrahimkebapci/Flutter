4af6dff8a5a2300a4feb76265676c6bb6b9f0886
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2024-03-16 17:41:20 +0300

    Update

---
+++ flutter_learn_full/fluttr_full_learn/android/app/src/main/kotlin/com/example/fluttr_full_learn/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fluttr_full_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_learn_full/fluttr_full_learn/lib/101/Text_learn.dart
@@ -0,0 +1,10 @@
+import 'package:flutt er/meteraial.dart';
+
+class TextLearnView extends StatelessWidget {
+
+@override
+Widget build(BuildContext context)
+{
+    return Text('Veli');
+}
+}
