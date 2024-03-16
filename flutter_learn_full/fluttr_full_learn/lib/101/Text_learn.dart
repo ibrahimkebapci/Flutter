53b08fc6662d92401aa0e9efb468757a1832fbc4
ibrahimkebapci <ibrahim_kebapci@hotmail.com>
2024-03-16 18:41:57 +0300

    Update

--- flutter_learn_full/fluttr_full_learn/lib/101/Text_learn.dart
+++ flutter_learn_full/fluttr_full_learn/lib/101/Text_learn.dart
@@ -5,6 +5,22 @@ class TextLearnView extends StatelessWidget {
 @override
 Widget build(BuildContext context)
 {
-    return Text('Veli');
+    return const SCaffold(
+        body: Center(
+            child: Text(
+                'Welcome '
+            ('Buy the best one'*10),
+            maxLines:2,
+            overflow: TextOverFlow.ellipsis,
+            textAlign: TextAlign.right,
+
+            style: TextStyle(wordSpacing: 2, letterSpacing: 2, fontSize:16,
+            fontWeight: fontWeight),
+            
+            
+            )),
+        );
+    )
+     const Text('Veli');
 }
 }
